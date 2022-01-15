3c335650e5fae0acf5cbbe61fee8ac52559ac039
Arpit Mhaisne <95077786+arpitmh25@users.noreply.github.com>
2022-01-15 23:05:12 +0530

    Update UserRegistrationTest.java
    Commiting Changes

--- src/test/java/com/bridgelabz/lambdaexpressions/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/lambdaexpressions/UserRegistrationTest.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.lambdaexpressions;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,7 +14,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.firstName("^[A-Z]{1}[a-z]{3,}$", "Niharika", "Happy");
+            result = userRegistration.firstName("^[A-Z]{1}[a-z]{3,}$", "Arpit", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -29,7 +28,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.firstName("^[A-Z]{1}[a-z]{3,}$", "niha", "Happy");
+            result = userRegistration.firstName("^[A-Z]{1}[a-z]{3,}$", "arpit", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -44,7 +43,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.lastName("^[A-Z]{1}[a-z]{3,}$", "Gowda", "Happy");
+            result = userRegistration.lastName("^[A-Z]{1}[a-z]{3,}$", "Patil", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -57,7 +56,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.lastName("^[A-Z]{1}[a-z]{3,}$", "kv", "Happy");
+            result = userRegistration.lastName("^[A-Z]{1}[a-z]{3,}$", "pa", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -71,7 +70,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.email("^[a-zA-Z0-9\\-\\+\\.]+.([a-zA-Z0-9])*@([a-z0-9]+.[a-z]{2,}.([a-z]{2,})?)$", "niha199831@gmail.com", "Happy");
+            result = userRegistration.email("^[a-zA-Z0-9\\-\\+\\.]+.([a-zA-Z0-9])*@([a-z0-9]+.[a-z]{2,}.([a-z]{2,})?)$", "arpitmh234@gmail.com", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -84,7 +83,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.email("^[a-zA-Z0-9\\-\\+\\.]+.([a-zA-Z0-9])*@([a-z0-9]+.[a-z]{2,}.([a-z]{2,})?)$", "niharika199831#gmail.com", "Happy");
+            result = userRegistration.email("^[a-zA-Z0-9\\-\\+\\.]+.([a-zA-Z0-9])*@([a-z0-9]+.[a-z]{2,}.([a-z]{2,})?)$", "arpitmh234#gmail.com", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -98,7 +97,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.phone("^[0-9]{2}[ ]?[0-9]{10}$", "917550167198", "Happy");
+            result = userRegistration.phone("^[0-9]{2}[ ]?[0-9]{10}$", "8888888888", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -111,7 +110,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.phone("^[0-9]{2}[ ]?[0-9]{10}$", "91550167198", "Happy");
+            result = userRegistration.phone("^[0-9]{2}[ ]?[0-9]{10}$", "8888845768", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -125,7 +124,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.password("^(?=.{8,}$)(?=.*\\d)(?=.*[A-Z])([a-zA-Z0-9]+[\\@\\#\\^])", "niha123A@", "Happy");
+            result = userRegistration.password("^(?=.{8,}$)(?=.*\\d)(?=.*[A-Z])([a-zA-Z0-9]+[\\@\\#\\^])", "arpit123@", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
@@ -138,7 +137,7 @@ public class UserRegistrationTest {
         try {
             InvalidUserException.ExpectedException expectedException = InvalidUserException.ExpectedException.none();
             expectedException.expect(InvalidUserException.class);
-            result = userRegistration.password("^(?=.{8,}$)(?=.*\\d)(?=.*[A-Z])([a-zA-Z0-9]+[\\@\\#\\^])", "niha123A", "Happy");
+            result = userRegistration.password("^(?=.{8,}$)(?=.*\\d)(?=.*[A-Z])([a-zA-Z0-9]+[\\@\\#\\^])", "arpit123A", "Happy");
         } catch (InvalidUserException e) {
             e.printStackTrace();
         }
